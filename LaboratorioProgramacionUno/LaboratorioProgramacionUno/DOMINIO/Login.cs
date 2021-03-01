adb37d84b530be2221fba0f173abc1b33b0c2097
kevin alexander <no0336032020@unab.edu.sv>
2021-02-28 22:28:27 -0600

    get y set

--- LaboratorioProgramacionUno/LaboratorioProgramacionUno/DOMINIO/Login.cs
+++ LaboratorioProgramacionUno/LaboratorioProgramacionUno/DOMINIO/Login.cs
@@ -8,5 +8,10 @@ namespace LaboratorioProgramacionUno.DOMINIO
 {
     class Login
     {
+        private String usuario;
+        private String password;
+
+        public string Usuario { get => usuario; set => usuario = value; }
+        public string Password { get => password; set => password = value; }
     }
 }
