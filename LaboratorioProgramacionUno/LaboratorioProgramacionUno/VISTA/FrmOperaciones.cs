5859d9be7d4727f7a71ad2e0a170c926a6f6d0fc
Denilson Martinez <mp0573032020@unab.edu.sv>
2021-03-01 00:04:56 -0600

    Actualizacion

---
+++ LaboratorioProgramacionUno/LaboratorioProgramacionUno/VISTA/FrmOperaciones.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaboratorioProgramacionUno.VISTA
+{
+    public partial class FrmOperaciones : Form
+    {
+        public FrmOperaciones()
+        {
+            InitializeComponent();
+        }
+        public String usuarioEstado;
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void txtOperaciones_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Operaciones_Load(object sender, EventArgs e)
+        {
+
+
+            label1.Text = usuarioEstado;
+
+        }
+
+    }
+    
+
+
+
